7ddc8ac6d399a62c96df6127c869cab4072460ff
Eredeti <93719013+Eredeti@users.noreply.github.com>
2023-11-16 16:28:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var hexrouge = 0xFF;
         var hexvert = 0x00;
         var hexbleu = 0x00;
-    
+
         function colorchanged() {
             // Mettre à jour les valeurs en fonction de certaines conditions
             if (hexrouge === 0xFF && hexvert < 0xFF && hexbleu === 0xFF) {
@@ -27,36 +27,35 @@ document.addEventListener("DOMContentLoaded", function() {
             if (hexrouge === 0xFF && hexvert === 0x00 && hexbleu > 0x00) {
                 hexbleu = hexbleu - 0x01;
             }
-    
+
             // Afficher les valeurs dans la console pour le débogage
             console.log("Rouge: " + hexrouge.toString(16));
             console.log("Vert: " + hexvert.toString(16));
             console.log("Bleu: " + hexbleu.toString(16));
-    
+
             // Construire la couleur hexadécimale finale
             var hexcouleur = "#" +
                 ("00" + hexrouge.toString(16)).slice(-2) +
                 ("00" + hexvert.toString(16)).slice(-2) +
                 ("00" + hexbleu.toString(16)).slice(-2);
-    
+
             // Afficher la couleur dans la console pour le débogage
             console.log("Couleur: " + hexcouleur);
-    
+
             // Appliquer la couleur de fond
             document.body.style.backgroundColor = hexcouleur;
         }
-    
+
         // Mettre à jour la couleur toutes les 1 seconde (1000 millisecondes)
         setInterval(colorchanged, 1000);
-    
+
         // Appeler la fonction une fois au chargement de la page
         colorchanged();
     })();
-    
+
     function updateMessage() {
-        var dynamicContent = document.getElementById("dynamicContent");
         var currentSeconds = new Date().getSeconds();
-    
+
         if (currentSeconds < 20) {
             dynamicContent.innerHTML = "Bonjour !";
         } else if (currentSeconds < 40) {

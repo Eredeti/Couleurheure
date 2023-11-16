fa644859695bdc12dd3e7fac073d0207f716aa25
Eredeti <93719013+Eredeti@users.noreply.github.com>
2023-11-16 15:24:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
     var dynamicContent = document.getElementById("dynamicContent");
-    var hexrouge = 0xFF;
-    var hexvert = 0x00;
-    var hexbleu = 0x00;
-    
-  function colorchanged() {
-    // Définir les valeurs hexadécimales initiales
 
-    // Mettre à jour les valeurs en fonction de certaines conditions
-    if (hexrouge === 0xFF && hexvert < 0xFF && hexbleu === 0xFF) {
-        hexvert = hexvert + 0x10;
-    }
-    if (hexrouge > 0x00 && hexvert === 0xFF && hexbleu === 0xFF) {
-        hexrouge = hexrouge - 0x10;
-    }
-    if (hexrouge === 0x00 && hexvert === 0xFF && hexbleu < 0xFF) {
-        hexbleu = hexbleu + 0x10;
-    }
-    if (hexrouge === 0x00 && hexvert > 0x00 && hexbleu === 0xFF) {
-        hexvert = hexvert - 0x10;
-    }
-    if (hexrouge < 0xFF && hexvert === 0x00 && hexbleu === 0xFF) {
-        hexrouge = hexrouge + 0x10;
-    }
-    if (hexrouge === 0xFF && hexvert === 0x00 && hexbleu > 0x00) {
-        hexbleu = hexbleu - 0x10;
-    }
+    // Utiliser une fonction auto-exécutante pour encapsuler les variables
+    (function() {
+        var hexrouge = 0xFF;
+        var hexvert = 0x00;
+        var hexbleu = 0x00;
 
-    // Construire la couleur hexadécimale finale
-    var hexcouleur = "#" + hexrouge.toString(16) + hexvert.toString(16) + hexbleu.toString(16);
+        function colorchanged() {
+            // Mettre à jour les valeurs en fonction de certaines conditions
+            if (hexrouge === 0xFF && hexvert < 0xFF && hexbleu === 0xFF) {
+                hexvert = hexvert + 0x10;
+            }
+            if (hexrouge > 0x00 && hexvert === 0xFF && hexbleu === 0xFF) {
+                hexrouge = hexrouge - 0x10;
+            }
+            if (hexrouge === 0x00 && hexvert === 0xFF && hexbleu < 0xFF) {
+                hexbleu = hexbleu + 0x10;
+            }
+            if (hexrouge === 0x00 && hexvert > 0x00 && hexbleu === 0xFF) {
+                hexvert = hexvert - 0x10;
+            }
+            if (hexrouge < 0xFF && hexvert === 0x00 && hexbleu === 0xFF) {
+                hexrouge = hexrouge + 0x10;
+            }
+            if (hexrouge === 0xFF && hexvert === 0x00 && hexbleu > 0x00) {
+                hexbleu = hexbleu - 0x10;
+            }
 
-    // Appliquer la couleur de fond
-    document.body.style.backgroundColor = hexcouleur;
-}
+            // Construire la couleur hexadécimale finale
+            var hexcouleur = "#" + hexrouge.toString(16) + hexvert.toString(16) + hexbleu.toString(16);
 
-    // Mettre à jour le message toutes les 1 secondes (1000 millisecondes)
-    setInterval(colorchanged, 1000);
+            // Appliquer la couleur de fond
+            document.body.style.backgroundColor = hexcouleur;
+        }
 
-    // Appeler la fonction une fois au chargement de la page
-    colorchanged();
+        // Mettre à jour la couleur toutes les 1 seconde (1000 millisecondes)
+        setInterval(colorchanged, 1000);
+
+        // Appeler la fonction une fois au chargement de la page
+        colorchanged();
+    })();
 });

533e32dc77bd8ae88b9d590dbbb60f2fe5508b4c
Eredeti <93719013+Eredeti@users.noreply.github.com>
2023-11-16 15:40:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,44 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Utiliser une fonction auto-exécutante pour encapsuler les variables
     (function() {
-        var hexrouge = 0xFF;
-        var hexvert = 0x00;
-        var hexbleu = 0x00;
+    var hexrouge = 0xFF;
+    var hexvert = 0x00;
+    var hexbleu = 0x00;
 
-        function colorchanged() {
-            // Mettre à jour les valeurs en fonction de certaines conditions
-            if (hexrouge === 0xFF && hexvert < 0xFF && hexbleu === 0xFF) {
-                hexvert = hexvert + 0x10;
-            }
-            if (hexrouge > 0x00 && hexvert === 0xFF && hexbleu === 0xFF) {
-                hexrouge = hexrouge - 0x10;
-            }
-            if (hexrouge === 0x00 && hexvert === 0xFF && hexbleu < 0xFF) {
-                hexbleu = hexbleu + 0x10;
-            }
-            if (hexrouge === 0x00 && hexvert > 0x00 && hexbleu === 0xFF) {
-                hexvert = hexvert - 0x10;
-            }
-            if (hexrouge < 0xFF && hexvert === 0x00 && hexbleu === 0xFF) {
-                hexrouge = hexrouge + 0x10;
-            }
-            if (hexrouge === 0xFF && hexvert === 0x00 && hexbleu > 0x00) {
-                hexbleu = hexbleu - 0x10;
-            }
+    function colorchanged() {
+        // Mettre à jour les valeurs en fonction de certaines conditions
+        hexrouge = (hexrouge + 0x10) % 0x100;
+        hexvert = (hexvert + 0x10) % 0x100;
+        hexbleu = (hexbleu + 0x10) % 0x100;
 
-            // Construire la couleur hexadécimale finale
-            var hexcouleur = "#" + hexrouge.toString(16) + hexvert.toString(16) + hexbleu.toString(16);
+        // Construire la couleur hexadécimale finale
+        var hexcouleur = "#" + hexrouge.toString(16) + hexvert.toString(16) + hexbleu.toString(16);
 
-            // Appliquer la couleur de fond
-            document.body.style.backgroundColor = hexcouleur;
-        }
+        // Appliquer la couleur de fond
+        document.body.style.backgroundColor = hexcouleur;
+    }
 
-        // Mettre à jour la couleur toutes les 1 seconde (1000 millisecondes)
-        setInterval(colorchanged, 1000);
+    // Mettre à jour la couleur toutes les 1 seconde (1000 millisecondes)
+    setInterval(colorchanged, 1000);
 
-        // Appeler la fonction une fois au chargement de la page
-        colorchanged();
-    })();
+    // Appeler la fonction une fois au chargement de la page
+    colorchanged();
+})();
 
     function updateMessage() {
         var dynamicContent = document.getElementById("dynamicContent");

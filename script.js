309071080181bd80c7ee430518851b8708c0f332
Eredeti <93719013+Eredeti@users.noreply.github.com>
2023-11-16 14:08:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function() {
         if (currentSecond < 20) {
             dynamicContent.innerHTML = "0-20 <br>" + currentSecond;
 
-
         } else if (currentSecond < 40) {
             dynamicContent.innerHTML = "20-40 <br>" + currentSecond;
         } else {
@@ -15,9 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    function colorchanged() {
+        var currentSecond = new Date().getSeconds();
+        var body = document.body;
+
+        if (currentSecond === 20) {
+            body.style.backgroundColor = "#FF0000";
+        } else if (currentSecond === 40) {
+            body.style.backgroundColor = "#00FF00";
+        } else if (currentSecond === 0) {
+            body.style.backgroundColor = "#00000FF";
+    }
+        
     // Mettre à jour le message toutes les 1 secondes (1000 millisecondes)
     setInterval(updateMessage, 1000);
-
+    setInterval(colorchanged, 1000);
+        
     // Appeler la fonction une fois au chargement de la page
     updateMessage();
+    colorchanged();
 });
